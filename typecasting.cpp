e7508bcd154cd25e415b3a1f21d4d7b722fb654a
Prashant Chaudhary <114799837+prashantkc09@users.noreply.github.com>
2023-05-08 23:02:05 +0530

    Add files via upload
    learning type casting

---
+++ typecasting.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+/*
+int main(){
+	
+	int a=123;
+	
+	cout<<a<<endl;
+	
+	char b='v';
+	
+	cout<<b<<endl;
+	
+	bool bl=true;
+	cout<<bl<<endl;
+	
+	float f=1.2;
+	cout<<f<<endl;
+	
+	double d=1.23;
+	cout<<d<<endl;
+	
+	int size=sizeof(d);
+	cout<<"size of a is:"<<size<<endl;
+}
+*/
+int a ='a';
+cout<< a <<endl;
+
+char ch=98;
+cout<<ch<<endl;
+
+char ch1=123456;
+cout<<ch1<<endl;
+}
+
