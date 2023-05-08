779bdcd252ea3b85a3307af2ec24cdbe31a43fc1
Prashant Chaudhary <114799837+prashantkc09@users.noreply.github.com>
2023-05-08 23:02:35 +0530

    Add files via upload
    how to store negative number

---
+++ storing -ve number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int a=2;
+int b=3;
+
+bool first =(a==b);
+cout<< first<<endl;
+
+bool second =(a>b);
+cout<< second<<endl;
+
+bool third =(a<b);
+cout<< third<<endl;
+}
